bed1d81ce452e2f4c080ff0ea647371e38276b5c
Calliernie <ernestamuzu@ymail.com>
2020-01-25 18:17:38 +0000

    update

--- src/JosWheel.java
+++ src/JosWheel.java
@@ -1,44 +1,49 @@
-/*code to allow user control over how many times he wants to
-continue running a program by giving specific input in the console*/
-
 import java.util.Scanner;
 
 public class JosWheel {
+    static Scanner keywordInput = new Scanner(System.in);
+
     public static void main(String[] args) {
-        re_run_keybd_input();
+        startProgram();
     }
 
-    private static void re_run_keybd_input() {
+    //method to start the whole program
+    public static void startProgram() {
         for (; ; ) {
             //initializations
-            Scanner keywordInput = new Scanner(System.in);
-            String keyboardInput_str = keywordInput.nextLine();
-
             String code_trigger_starter_str = "start";
             String code_trigger_ender_str = "end";
+            String prompt = "Enter start to run the program";
 
-            int x = 0;
-            int y = 5;
-
-        /*setting an if condition that determines when to start or stop
-            the program based on user input*/
+            System.out.println(prompt);
+            String keyboardInput_str = keywordInput.nextLine();
 
             if (keyboardInput_str.equalsIgnoreCase(code_trigger_starter_str)) {
-                //while loop used to print we are still in the while loop five times
-                while (x < y) {
-                    System.out.println("we are still in the while loop: " + Integer.toString(x));
-                    x = x + 1;
-                }
-                //code to print we are out of while loop after the while loop ends
-                System.out.println("We are out of the while loop");
-                System.out.println();
+                keepTyping();
+                break;
             } else if (keyboardInput_str.equalsIgnoreCase(code_trigger_ender_str)) {
-                //else if clause that executes when user enters end in the console
-                System.out.println("GOODBYE!!!");
+                System.out.println("Program has ended");
                 break;
             } else {
-                //code to run when user enters any other input aside start or end
-                System.out.println("Please enter the correct code to run the program");
+                //System.out.println(prompt);
+            }
+        }
+    }
+
+    //method that keeps printing what users types and ends when he types end
+    public static void keepTyping() {
+        for (; ; ) {
+            String code_trigger_ender_str = "end";
+            String userInput = keywordInput.nextLine();
+
+            if (userInput.equalsIgnoreCase(null) || userInput.length() == 0) {
+                userInput = "Invalid input, enter a string to continue";
+                System.out.println(userInput);
+            } else if (!userInput.equalsIgnoreCase(code_trigger_ender_str)) {
+                System.out.println(userInput);
+            } else {
+                System.out.println("Program has ended");
+                break;
             }
         }
     }
